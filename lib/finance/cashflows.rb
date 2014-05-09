0e40a12bc2f339e8dc5e183b692fbc898afe0e22
tubedude <beto@trevisan.me>
2014-05-09 00:17:07 -0300

    Added support for configuration file
    Will allow to configure eps and guess in a configuration file in config/initializers

--- lib/finance/cashflows.rb
+++ lib/finance/cashflows.rb
@@ -14,7 +14,7 @@ module Finance
     # @api private
     class Function
       values = {
-        eps: "1.0e-16",
+        eps: Finance.config.eps,
         one: "1.0",
         two: "2.0",
         ten: "10.0",
@@ -50,7 +50,7 @@ module Finance
     #   Finance::Cashflow.new([-4000,1200,1410,1875,1050]).irr #=> 0.143
     # @see http://en.wikipedia.org/wiki/Internal_rate_of_return
     # @api public
-    def irr(guess=1.0)
+    def irr(guess=nil)
       # Make sure we have a valid sequence of cash flows.
       positives, negatives = @__cash_flows.partition{ |i| i >= 0 }
       if positives.empty? || negatives.empty?
@@ -58,7 +58,7 @@ module Finance
       end
 
       func = Function.new(@__cash_flows, :npv)
-      rate = [ Flt::DecNum.new(guess.to_s) ] if guess.is_a? Numeric
+      rate = [ valid(guess) ]
       nlsolve( func, rate )
       rate[0]
     end
@@ -95,7 +95,7 @@ module Finance
     #   @transactions << Transaction.new(  600, :date => Time.new(1995,01,01))
     #   Finance::Cashflow.new(@transactions).xirr(0.6) #=> Rate("0.024851", :apr, :compounds => :annually)
     # @api public
-    def xirr(guess=1.0)
+    def xirr(guess=nil)
       # Make sure we have a valid sequence of cash flows.
       positives, negatives = @__cash_flows.partition{ |t| t.amount >= 0 }
       if positives.empty? || negatives.empty?
@@ -103,7 +103,7 @@ module Finance
       end
 
       func = Function.new(@__cash_flows, :xnpv)
-      rate = [ Flt::DecNum.new(guess.to_s) ] if guess.is_a? Numeric
+      rate = [ valid(guess) ]
       nlsolve( func, rate )
       Rate.new(rate[0], :apr, :compounds => :annually)
     end
@@ -134,6 +134,18 @@ module Finance
     def [](*args)
       cash_flows = @__cash_flows[*args]
       Finance::Cashflow.new(cash_flows)
+    end
+
+    private
+    def valid(guess)
+      result = if guess.nil?
+        raise ArgumentError, "Invalid Guess. Default guess should be a [Numeric] value." unless Finance.config.guess.is_a? Numeric
+        Finance.config.guess
+      else
+        raise ArgumentError, "Invalid Guess. Use a [Numeric] value." unless guess.is_a? Numeric
+        guess
+      end
+      return Flt::DecNum.new(result.to_s)
     end
   end
 end
