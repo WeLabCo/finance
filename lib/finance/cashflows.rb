92bc9d7b509606ebb5c906903711fb4922487f79
tubedude <beto@trevisan.me>
2014-05-08 22:53:12 -0300

    Guess Params
    I've changed it even further to be more direct to the point. I still consider 1.0 to be a high default. It would be nice if this default could be set in a Initialized Config param.

--- lib/finance/cashflows.rb
+++ lib/finance/cashflows.rb
@@ -45,12 +45,12 @@ module Finance
 
     # calculate the internal rate of return for a sequence of cash flows
     # @return [DecNum] the internal rate of return
-    # @param [Numeric] Initial guess rate
+    # @param [Numeric] Initial guess rate, Defaults to 1.0
     # @example
     #   Finance::Cashflow.new([-4000,1200,1410,1875,1050]).irr #=> 0.143
     # @see http://en.wikipedia.org/wiki/Internal_rate_of_return
     # @api public
-    def irr(guess=nil)
+    def irr(guess=1.0)
       # Make sure we have a valid sequence of cash flows.
       positives, negatives = @__cash_flows.partition{ |i| i >= 0 }
       if positives.empty? || negatives.empty?
@@ -58,7 +58,7 @@ module Finance
       end
 
       func = Function.new(@__cash_flows, :npv)
-      rate = guess.nil? ? [ func.one ] : [ guess.to_f ]
+      rate = [ Flt::DecNum.new(guess.to_s) ] if guess.is_a? Numeric
       nlsolve( func, rate )
       rate[0]
     end
@@ -86,7 +86,7 @@ module Finance
     end
 
     # calculate the internal rate of return for a sequence of cash flows with dates
-    # @param[Numeric] Initial guess rate
+    # @param[Numeric] Initial guess rate, Deafults to 1.0
     # @return [Rate] the internal rate of return
     # @example
     #   @transactions = []
@@ -95,7 +95,7 @@ module Finance
     #   @transactions << Transaction.new(  600, :date => Time.new(1995,01,01))
     #   Finance::Cashflow.new(@transactions).xirr(0.6) #=> Rate("0.024851", :apr, :compounds => :annually)
     # @api public
-    def xirr(guess=nil)
+    def xirr(guess=1.0)
       # Make sure we have a valid sequence of cash flows.
       positives, negatives = @__cash_flows.partition{ |t| t.amount >= 0 }
       if positives.empty? || negatives.empty?
@@ -103,7 +103,7 @@ module Finance
       end
 
       func = Function.new(@__cash_flows, :xnpv)
-      rate = guess.nil? ? [ func.one ] : [ guess.to_f ]
+      rate = [ Flt::DecNum.new(guess.to_s) ] if guess.is_a? Numeric
       nlsolve( func, rate )
       Rate.new(rate[0], :apr, :compounds => :annually)
     end
